10b5fcddb2bfd2f1daeafc0b5c928b0ed233ff51
Roman Revin <xgismox@gmail.com>
2014-07-28 17:21:41 +0400

    Up

---
+++ HtmlCompressor.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * HtmlCompressor.php
+ * @author Revin Roman http://phptime.ru
+ */
+
+namespace rmrevin\yii\minify;
+
+class HtmlCompressor
+{
+    public static function make($data, $options = null)
+    {
+        return html_compress($data, $options);
+    }
+}
+
+// HTML Compressor 1.0.0
+// Original Author: Tyler Hall <tylerhall@gmail.com>
+// Latest Source and Bug Tracker: http://github.com/tylerhall/html-compressor
+//
+// Attemps to reduce the filesize of an HTML document by removing unnecessary
+// whitespace at the beginning and end of lines, inside closing tags, and
+// stripping blank lines completely. <pre> tags are respected and their contents
+// are left alone. Warning, nested <pre> tags may exhibit unexpected behaviour.
+//
+// This code is licensed under the MIT Open Source License.
+// Copyright (c) 2010 tylerhall@gmail.com
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// $data is either a handle to an open file, or an HTML string
+function html_compress($data, $options = null)
+{
+    if (!isset($options)) {
+        $options = array();
+    }
+
+    $data .= "\n";
+    $out = '';
+    $inside_pre = false;
+    $bytecount = 0;
+
+    while ($line = get_line($data)) {
+        $bytecount += strlen($line);
+
+        if (!$inside_pre) {
+            if (strpos($line, '<pre') === false) {
+                // Since we're not inside a <pre> block, we can trim both ends of the line
+                $line = trim($line);
+
+                // And condense multiple spaces down to one
+                $line = preg_replace('/\s\s+/', ' ', $line);
+            } else {
+                // Only trim the beginning since we just entered a <pre> block...
+                $line = ltrim($line);
+                $inside_pre = true;
+
+                // If the <pre> ends on the same line, don't turn on $inside_pre...
+                if ((strpos($line, '</pre') !== false) && (strripos($line, '</pre') >= strripos($line, '<pre'))) {
+                    $line = rtrim($line);
+                    $inside_pre = false;
+                }
+            }
+        } else {
+            if ((strpos($line, '</pre') !== false) && (strripos($line, '</pre') >= strripos($line, '<pre'))) {
+                // Trim the end of the line now that we found the end of the <pre> block...
+                $line = rtrim($line);
+                $inside_pre = false;
+            }
+        }
+
+        // Filter out any blank lines that aren't inside a <pre> block...
+        if ($inside_pre || $line != '') {
+            $out .= $line . "\n";
+        }
+    }
+
+    // Remove HTML comments...
+    if (array_key_exists('c', $options) || array_key_exists('no-comments', $options)) {
+        $out = preg_replace('/(<!--.*?-->)/ms', '', $out);
+        $out = str_replace('<!>', '', $out);
+    }
+
+    // Perform any extra (unsafe) compression techniques...
+    if (array_key_exists('x', $options) || array_key_exists('extra', $options)) {
+        // Can break layouts that are dependent on whitespace between tags
+        $out = str_replace(">\n<", '><', $out);
+    }
+
+    // Remove the trailing \n
+    $out = trim($out);
+
+    // Output either our stats or the compressed data...
+    if (array_key_exists('s', $options) || array_key_exists('stats', $options)) {
+        $echo = '';
+        $echo .= "Original Size: $bytecount\n";
+        $echo .= "Compressed Size: " . strlen($out) . "\n";
+        $echo .= "Savings: " . round((1 - strlen($out) / $bytecount) * 100, 2) . "%\n";
+        echo $echo;
+    } else {
+        if (array_key_exists('o', $options) || array_key_exists('overwrite', $options)) {
+            if ($GLOBALS['argc'] > 1 && is_writable($GLOBALS['argv'][$GLOBALS['argc'] - 1])) {
+                file_put_contents($GLOBALS['argv'][$GLOBALS['argc'] - 1], $out);
+
+                return true;
+            } else {
+                return "Error: could not write to " . $GLOBALS['argv'][$GLOBALS['argc'] - 1] . "\n";
+            }
+        } else {
+            return $out;
+        }
+    }
+
+    return false;
+}
+
+// Returns the next line from an open file handle or a string
+function get_line(&$data)
+{
+    if (is_resource($data)) {
+        return fgets($data);
+    }
+
+    if (is_string($data)) {
+        if (strlen($data) > 0) {
+            $pos = strpos($data, "\n");
+            $return = substr($data, 0, $pos) . "\n";
+            $data = substr($data, $pos + 1);
+
+            return $return;
+        } else {
+            return false;
+        }
+    }
+
+    return false;
+}
--- View.php
+++ View.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * View.php
- * @author Roman Revim
- * @link http://phptime.ru
+ * @author Revin Roman http://phptime.ru
  */
 
 namespace rmrevin\yii\minify;
@@ -14,270 +13,274 @@ use yii\helpers\StringHelper;
 class View extends \yii\web\View
 {
 
-	/**
-	 * @var string path alias to web base
-	 */
-	public $base_path = '@app/web';
+    /**
+     * @var string path alias to web base
+     */
+    public $base_path = '@app/web';
 
-	/**
-	 * @var string path alias to save minify result
-	 */
-	public $minify_path = '@app/web/minify';
+    /**
+     * @var string path alias to save minify result
+     */
+    public $minify_path = '@app/web/minify';
 
-	/**
-	 * @var bool|string charset forcibly assign, otherwise will use all of the files found charset
-	 */
-	public $force_charset = false;
+    /**
+     * @var bool|string charset forcibly assign, otherwise will use all of the files found charset
+     */
+    public $force_charset = false;
 
-	/**
-	 * @var bool whether to change @import on content
-	 */
-	public $expand_imports = true;
+    /**
+     * @var bool whether to change @import on content
+     */
+    public $expand_imports = true;
 
-	/**
-	 * @var int
-	 */
-	public $css_linebreak_pos = 2048;
+    /**
+     * @var int
+     */
+    public $css_linebreak_pos = 2048;
 
-	/**
-	 * @var int chmod of minified file
-	 */
-	public $file_mode = 0664;
+    /**
+     * @var int chmod of minified file
+     */
+    public $file_mode = 0664;
 
-	/**
-	 * @var array schemes that will be ignored during normalization url
-	 */
-	public $schemas = ['//', 'http://', 'https://', 'ftp://'];
+    /**
+     * @var array schemes that will be ignored during normalization url
+     */
+    public $schemas = ['//', 'http://', 'https://', 'ftp://'];
 
-	public function init()
-	{
-		parent::init();
+    public function init()
+    {
+        parent::init();
 
-		$minify_path = $this->minify_path = \Yii::getAlias($this->minify_path);
-		if (!file_exists($minify_path)) {
-			FileHelper::createDirectory($minify_path);
-		}
+        $minify_path = $this->minify_path = \Yii::getAlias($this->minify_path);
+        if (!file_exists($minify_path)) {
+            FileHelper::createDirectory($minify_path);
+        }
 
-		if (!is_readable($minify_path)) {
-			throw new \RuntimeException(\Yii::t('app', 'Directory for compressed assets is not readable.'));
-		}
+        if (!is_readable($minify_path)) {
+            throw new \RuntimeException(\Yii::t('app', 'Directory for compressed assets is not readable.'));
+        }
 
-		if (!is_writable($minify_path)) {
-			throw new \RuntimeException(\Yii::t('app', 'Directory for compressed assets is not writable.'));
-		}
-	}
+        if (!is_writable($minify_path)) {
+            throw new \RuntimeException(\Yii::t('app', 'Directory for compressed assets is not writable.'));
+        }
+    }
 
-	public function endPage($ajaxMode = false)
-	{
-		$this->trigger(self::EVENT_END_PAGE);
+    public function endPage($ajaxMode = false)
+    {
+        $this->trigger(self::EVENT_END_PAGE);
 
-		$content = ob_get_clean();
-		foreach (array_keys($this->assetBundles) as $bundle) {
-			$this->registerAssetFiles($bundle);
-		}
+        $content = ob_get_clean();
+        foreach (array_keys($this->assetBundles) as $bundle) {
+            $this->registerAssetFiles($bundle);
+        }
 
-		$this->minify();
+        $this->minify();
 
-		echo strtr($content, [
-			self::PH_HEAD => $this->renderHeadHtml(),
-			self::PH_BODY_BEGIN => $this->renderBodyBeginHtml(),
-			self::PH_BODY_END => $this->renderBodyEndHtml($ajaxMode),
-		]);
+        echo strtr(
+            $content,
+            [
+                self::PH_HEAD => $this->renderHeadHtml(),
+                self::PH_BODY_BEGIN => $this->renderBodyBeginHtml(),
+                self::PH_BODY_END => $this->renderBodyEndHtml($ajaxMode),
+            ]
+        );
 
-		$this->clear();
-	}
+        $this->clear();
+    }
 
-	private function registerAssetFiles($name)
-	{
-		if (!isset($this->assetBundles[$name]))
-			return;
+    protected function registerAssetFiles($name)
+    {
+        if (!isset($this->assetBundles[$name])) {
+            return;
+        }
 
-		$bundle = $this->assetBundles[$name];
-		if ($bundle) {
-			foreach ($bundle->depends as $dep) {
-				$this->registerAssetFiles($dep);
-			}
+        $bundle = $this->assetBundles[$name];
+        if ($bundle) {
+            foreach ($bundle->depends as $dep) {
+                $this->registerAssetFiles($dep);
+            }
 
-			$bundle->registerAssetFiles($this);
-		}
+            $bundle->registerAssetFiles($this);
+        }
 
-		unset($this->assetBundles[$name]);
-	}
+        unset($this->assetBundles[$name]);
+    }
 
-	private function minify()
-	{
-		$this
-			->minifyCSS()
-			->minifyJS();
-	}
+    private function minify()
+    {
+        $this
+            ->minifyCSS()
+            ->minifyJS();
+    }
 
-	/**
-	 * @return self
-	 */
-	private function minifyCSS()
-	{
-		if (!empty($this->cssFiles)) {
-			$css_files = array_keys($this->cssFiles);
+    /**
+     * @return self
+     */
+    private function minifyCSS()
+    {
+        if (!empty($this->cssFiles)) {
+            $css_files = array_keys($this->cssFiles);
 
-			$long_hash = '';
-			foreach ($css_files as $file) {
-				$file = \Yii::getAlias($this->base_path) . $file;
-				$hash = sha1_file($file);
-				$long_hash .= $hash;
-			}
+            $long_hash = '';
+            foreach ($css_files as $file) {
+                $file = \Yii::getAlias($this->base_path) . $file;
+                $hash = sha1_file($file);
+                $long_hash .= $hash;
+            }
 
-			$css_minify_file = $this->minify_path . DIRECTORY_SEPARATOR . sha1($long_hash) . '.css';
-			if (!file_exists($css_minify_file)) {
-				$css = '';
-				$charsets = '';
-				$imports = '';
-				$fonts = '';
+            $css_minify_file = $this->minify_path . DIRECTORY_SEPARATOR . sha1($long_hash) . '.css';
+            if (!file_exists($css_minify_file)) {
+                $css = '';
+                $charsets = '';
+                $imports = '';
+                $fonts = '';
 
-				foreach ($css_files as $file) {
-					$content = file_get_contents(\Yii::getAlias($this->base_path) . $file);
+                foreach ($css_files as $file) {
+                    $content = file_get_contents(\Yii::getAlias($this->base_path) . $file);
 
-					preg_match_all('|url\(([^)]+)\)|is', $content, $m);
-					if (!empty($m[0])) {
-						$path = dirname($file);
-						$result = [];
-						foreach ($m[0] as $k => $v) {
-							$url = str_replace(['\'', '"'], '', $m[1][$k]);
-							if (preg_match('#^(' . implode('|', $this->schemas) . ')#is', $url)) {
-								$result[$m[1][$k]] = '\'' . $url . '\'';
-							} else {
-								$result[$m[1][$k]] = '\'' . $path . DIRECTORY_SEPARATOR . $url . '\'';
-							}
-						}
-						$content = str_replace(array_keys($result), array_values($result), $content);
-					}
+                    preg_match_all('|url\(([^)]+)\)|is', $content, $m);
+                    if (!empty($m[0])) {
+                        $path = dirname($file);
+                        $result = [];
+                        foreach ($m[0] as $k => $v) {
+                            $url = str_replace(['\'', '"'], '', $m[1][$k]);
+                            if (preg_match('#^(' . implode('|', $this->schemas) . ')#is', $url)) {
+                                $result[$m[1][$k]] = '\'' . $url . '\'';
+                            } else {
+                                $result[$m[1][$k]] = '\'' . $path . DIRECTORY_SEPARATOR . $url . '\'';
+                            }
+                        }
+                        $content = str_replace(array_keys($result), array_values($result), $content);
+                    }
 
-					$css .= $content;
-				}
+                    $css .= $content;
+                }
 
-				if (true === $this->expand_imports) {
-					preg_match_all('|\@import\s([^;]+);|is', $css, $m);
-					if (!empty($m[0])) {
-						foreach ($m[0] as $k => $v) {
-							$import_url = $m[1][$k];
-							if (!empty($import_url)) {
-								$import_content = $this->getImportContent($import_url);
-								if (!empty($import_content)) {
-									$css = str_replace($m[0][$k], $import_content, $css);
-								}
-							}
-						}
-					}
-				}
+                if (true === $this->expand_imports) {
+                    preg_match_all('|\@import\s([^;]+);|is', $css, $m);
+                    if (!empty($m[0])) {
+                        foreach ($m[0] as $k => $v) {
+                            $import_url = $m[1][$k];
+                            if (!empty($import_url)) {
+                                $import_content = $this->getImportContent($import_url);
+                                if (!empty($import_content)) {
+                                    $css = str_replace($m[0][$k], $import_content, $css);
+                                }
+                            }
+                        }
+                    }
+                }
 
-				$css = (new \CSSmin())->run($css, $this->css_linebreak_pos);
+                $css = (new \CSSmin())->run($css, $this->css_linebreak_pos);
 
-				if (false !== $this->force_charset) {
-					$charsets = '@charset "' . (string)$this->force_charset . '";' . PHP_EOL;
-				}
+                if (false !== $this->force_charset) {
+                    $charsets = '@charset "' . (string)$this->force_charset . '";' . PHP_EOL;
+                }
 
-				preg_match_all('|\@charset[^;]+|is', $css, $m);
-				if (!empty($m[0])) {
-					foreach ($m[0] as $k => $v) {
-						$string = $m[0][$k] . ';';
-						$css = str_replace($string, '', $css);
-						if (false === $this->force_charset) {
-							$charsets .= $string . PHP_EOL;
-						}
-					}
-				}
+                preg_match_all('|\@charset[^;]+|is', $css, $m);
+                if (!empty($m[0])) {
+                    foreach ($m[0] as $k => $v) {
+                        $string = $m[0][$k] . ';';
+                        $css = str_replace($string, '', $css);
+                        if (false === $this->force_charset) {
+                            $charsets .= $string . PHP_EOL;
+                        }
+                    }
+                }
 
-				preg_match_all('|\@import[^;]+|is', $css, $m);
-				if (!empty($m[0])) {
-					foreach ($m[0] as $k => $v) {
-						$string = $m[0][$k] . ';';
-						$imports .= $string . PHP_EOL;
-						$css = str_replace($string, '', $css);
-					}
-				}
+                preg_match_all('|\@import[^;]+|is', $css, $m);
+                if (!empty($m[0])) {
+                    foreach ($m[0] as $k => $v) {
+                        $string = $m[0][$k] . ';';
+                        $imports .= $string . PHP_EOL;
+                        $css = str_replace($string, '', $css);
+                    }
+                }
 
-				preg_match_all('|\@font-face\{[^}]+\}|is', $css, $m);
-				if (!empty($m[0])) {
-					foreach ($m[0] as $k => $v) {
-						$string = $m[0][$k];
-						$fonts .= $string . PHP_EOL;
-						$css = str_replace($string, '', $css);
-					}
-				}
+                preg_match_all('|\@font-face\{[^}]+\}|is', $css, $m);
+                if (!empty($m[0])) {
+                    foreach ($m[0] as $k => $v) {
+                        $string = $m[0][$k];
+                        $fonts .= $string . PHP_EOL;
+                        $css = str_replace($string, '', $css);
+                    }
+                }
 
-				$css = $charsets . $imports . $fonts . $css;
+                $css = $charsets . $imports . $fonts . $css;
 
-				file_put_contents($css_minify_file, $css);
-				chmod($css_minify_file, $this->file_mode);
-			}
+                file_put_contents($css_minify_file, $css);
+                chmod($css_minify_file, $this->file_mode);
+            }
 
-			$css_file = str_replace(\Yii::getAlias($this->base_path), '', $css_minify_file);
-			$this->cssFiles = [$css_file => Html::cssFile($css_file)];
-		}
+            $css_file = str_replace(\Yii::getAlias($this->base_path), '', $css_minify_file);
+            $this->cssFiles = [$css_file => Html::cssFile($css_file)];
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return self
-	 */
-	private function minifyJS()
-	{
-		if (!empty($this->jsFiles)) {
-			$only_pos = [self::POS_END];
-			$js_files = $this->jsFiles;
-			foreach ($js_files as $position => $files) {
-				if (false === in_array($position, $only_pos)) {
-					$this->jsFiles[$position] = [];
-					foreach ($files as $file => $html) {
-						$this->jsFiles[$position][$file] = Html::jsFile($file);
-					}
-				} else {
-					$this->jsFiles[$position] = [];
+    /**
+     * @return self
+     */
+    private function minifyJS()
+    {
+        if (!empty($this->jsFiles)) {
+            $only_pos = [self::POS_END];
+            $js_files = $this->jsFiles;
+            foreach ($js_files as $position => $files) {
+                if (false === in_array($position, $only_pos)) {
+                    $this->jsFiles[$position] = [];
+                    foreach ($files as $file => $html) {
+                        $this->jsFiles[$position][$file] = Html::jsFile($file);
+                    }
+                } else {
+                    $this->jsFiles[$position] = [];
 
-					$long_hash = '';
-					foreach ($files as $file => $html) {
-						$file = \Yii::getAlias($this->base_path) . $file;
-						$hash = sha1_file($file);
-						$long_hash .= $hash;
-					}
+                    $long_hash = '';
+                    foreach ($files as $file => $html) {
+                        $file = \Yii::getAlias($this->base_path) . $file;
+                        $hash = sha1_file($file);
+                        $long_hash .= $hash;
+                    }
 
-					$js_minify_file = $this->minify_path . DIRECTORY_SEPARATOR . sha1($long_hash) . '.js';
-					if (!file_exists($js_minify_file)) {
-						$js = '';
-						foreach ($files as $file => $html) {
-							$file = \Yii::getAlias($this->base_path) . $file;
-							$js .= file_get_contents($file) . ';' . PHP_EOL;
-						}
+                    $js_minify_file = $this->minify_path . DIRECTORY_SEPARATOR . sha1($long_hash) . '.js';
+                    if (!file_exists($js_minify_file)) {
+                        $js = '';
+                        foreach ($files as $file => $html) {
+                            $file = \Yii::getAlias($this->base_path) . $file;
+                            $js .= file_get_contents($file) . ';' . PHP_EOL;
+                        }
 
-						$js = (new \JSMin($js))->min();
+                        $js = (new \JSMin($js))->min();
 
-						file_put_contents($js_minify_file, $js);
-						chmod($js_minify_file, $this->file_mode);
-					}
+                        file_put_contents($js_minify_file, $js);
+                        chmod($js_minify_file, $this->file_mode);
+                    }
 
-					$js_file = str_replace(\Yii::getAlias($this->base_path), '', $js_minify_file);
-					$this->jsFiles[$position][$js_file] = Html::jsFile($js_file);
-				}
-			}
-		}
+                    $js_file = str_replace(\Yii::getAlias($this->base_path), '', $js_minify_file);
+                    $this->jsFiles[$position][$js_file] = Html::jsFile($js_file);
+                }
+            }
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	private function getImportContent($url)
-	{
-		$result = null;
+    private function getImportContent($url)
+    {
+        $result = null;
 
-		if ('url(' === StringHelper::byteSubstr($url, 0, 4)) {
-			$url = str_replace(['url(\'', 'url(', '\')', ')'], '', $url);
+        if ('url(' === StringHelper::byteSubstr($url, 0, 4)) {
+            $url = str_replace(['url(\'', 'url(', '\')', ')'], '', $url);
 
-			if (StringHelper::byteSubstr($url, 0, 2) === '//')
-				$url = preg_replace('|^//|', 'http://', $url, 1);
+            if (StringHelper::byteSubstr($url, 0, 2) === '//')
+                $url = preg_replace('|^//|', 'http://', $url, 1);
 
-			if (!empty($url))
-				$result = file_get_contents($url);
-		}
+            if (!empty($url))
+                $result = file_get_contents($url);
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 }
